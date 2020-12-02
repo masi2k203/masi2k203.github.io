7f6f57bfd85982c635e35c1b0a7d4fd564cf2e06
masi2k203 <57385580+masi2k203@users.noreply.github.com>
2020-12-02 17:48:18 +0900

    とりあえず名前をターミナルサイトにしておいた

--- common/NavigationBar.js
+++ common/NavigationBar.js
@@ -1,7 +1,7 @@
 var htmlCode = 
 '<div id="NavBar">' +
     '<nav class="navbar navbar-dark bg-dark">' +
-        '<a href="/index.html" class="navbar-brand"><strong>メモ書きメインのページ</strong></a>' +
+        '<a href="/index.html" class="navbar-brand"><strong>ターミナルサイト</strong></a>' +
     '</nav>' +
 '</div>' 
 document.write(htmlCode)
