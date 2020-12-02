818ee6efa630c6702dd8b662780172a564fc7b40
masi2k203 <57385580+masi2k203@users.noreply.github.com>
2020-12-02 16:46:14 +0900

    ナビゲーションバー・フッターの共通化

---
+++ common/Footer.js
@@ -0,0 +1,17 @@
+var htmlCode =
+'<footer class="fixed-bottom">' +
+'<nav class="navbar navbar-expand navbar-dark bg-info">' +
+    '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navmenu1" aria-controls="navmenu1" aria-expanded="false" aria-label="Toggle navigation">' +
+        '<span class="navbar-toggler-icon"></span>' +
+    '</button>' +
+    '<div class="collapse navbar-collapse" id="navmenu1">' +
+        '<div class="navbar-nav">' +
+            '<a class="nav-item nav-link" href="/index.html">TOP</a>' +
+            '<a class="nav-item nav-link" href="/Application/index.html">作り置き</a>' +
+            '<a class="nav-item nav-link" href="/Note/index.html">Note</a>' +
+        '</div>' +
+    '</div>' +
+'</nav>' +
+'</footer>'
+
+document.write(htmlCode)
---
+++ common/NavigationBar.js
@@ -0,0 +1,7 @@
+var htmlCode = 
+'<div id="NavBar">' +
+    '<nav class="navbar navbar-dark bg-dark mb-2">' +
+        '<a href="/index.html" class="navbar-brand">メモ書きメインのページ</a>' +
+    '</nav>' +
+'</div>' 
+document.write(htmlCode)
