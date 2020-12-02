768475cdf10e78e0daf44c83b1fcce287dee7d24
masi2k203 <57385580+masi2k203@users.noreply.github.com>
2020-12-02 17:37:29 +0900

    メインページ変更 ナビゲーションバー文字を太くした

--- common/NavigationBar.js
+++ common/NavigationBar.js
@@ -1,7 +1,7 @@
 var htmlCode = 
 '<div id="NavBar">' +
-    '<nav class="navbar navbar-dark bg-dark mb-2">' +
-        '<a href="/index.html" class="navbar-brand">メモ書きメインのページ</a>' +
+    '<nav class="navbar navbar-dark bg-dark">' +
+        '<a href="/index.html" class="navbar-brand"><strong>メモ書きメインのページ</strong></a>' +
     '</nav>' +
 '</div>' 
 document.write(htmlCode)
